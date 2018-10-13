1bede99dd1605ef181a83d45ee99ca584bfe03ee
srikanthpragada <srikanthpragada@gmail.com>
2018-10-13 09:04:16 +0530

    13-OCT

---
+++ src/main/java/com/st/springdemo/Hello.java
@@ -0,0 +1,12 @@
+package com.st.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Hello {
+    
+	public String getMessage() {
+		return "Hello Spring";
+	}
+	
+}
